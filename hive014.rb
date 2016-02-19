6a13531814f0bebcd22965d14b545b27ecdcf355
Jose Maria Nunez-Izu <jmnunezizu@gmail.com>
2016-02-19 14:34:09 +0000

    Add Hive 0.14 formula.

---
+++ hive014.rb
@@ -0,0 +1,25 @@
+class Hive014 < Formula
+  desc "Hadoop-based data summarization, query, and analysis"
+  homepage "https://hive.apache.org"
+  url "https://archive.apache.org/dist/hive/hive-0.14.0/apache-hive-0.14.0-bin.tar.gz"
+  sha256 "7db1d3187f8bde20adbb59c37f536339d2548b39d9974508bd3a813e48d7b61c"
+
+  depends_on "hadoop"
+  depends_on :java
+
+  def install
+    rm_f Dir["bin/*.bat"]
+    libexec.install %w[bin conf examples lib ]
+    libexec.install Dir["*.jar"]
+    bin.write_exec_script Dir["#{libexec}/bin/*"]
+  end
+
+  def caveats; <<-EOS.undent
+    Hadoop must be in your path for hive executable to work.
+    After installation, set $HIVE_HOME in your profile:
+      export HIVE_HOME=#{libexec}
+    You may need to set JAVA_HOME:
+      export JAVA_HOME="$(/usr/libexec/java_home)"
+    EOS
+  end
+end
